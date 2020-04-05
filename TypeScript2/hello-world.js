c72c125aa173b8ec23303785e44098497d1d8b47
raduceaca1234 <ceaca.radu@yahoo.com>
2020-04-06 02:34:31 +0300

    Started the second tutorial of ts
    Examples of primitive types in ts

---
+++ TypeScript2/hello-world.js
@@ -0,0 +1,14 @@
+//arrow fct
+var fn = function () { return 'response'; };
+//js
+var a;
+a = 10;
+a = true;
+a = {};
+//ts
+var a2;
+var b;
+var c;
+a2 = 10;
+b = true;
+c = 'Hello';
---
+++ TypeScript2/hello-world.ts
@@ -0,0 +1,20 @@
+
+//arrow fct
+var fn = () => 'response';
+
+//js
+var a;
+a =10;
+a = true;
+a = {};
+
+//ts
+var a2: number;
+var b: boolean;
+var c: string;
+a2=10;
+b = true;
+c = 'Hello';
+
+
+
